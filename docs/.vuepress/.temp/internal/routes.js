b20cb2534fe1cf607eaef2d2c05e627f6f434677
wlphp <613154514@qq.com>
2026-02-09 15:09:46 +0800

    11
    11

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -18,20 +18,3 @@ export const routes = Object.fromEntries([
   ["/guide/register.html", { loader: () => import(/* webpackChunkName: "guide_register.html" */"D:/wwwroot/wanglei/ccnight_vuepress/docs/.vuepress/.temp/pages/guide/register.html.js"), meta: {"title":"注册账号"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"D:/wwwroot/wanglei/ccnight_vuepress/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateRoutes) {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-  }
-  if (__VUE_HMR_RUNTIME__.updateRedirects) {
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ routes, redirects }) => {
-    __VUE_HMR_RUNTIME__.updateRoutes(routes)
-    __VUE_HMR_RUNTIME__.updateRedirects(redirects)
-  })
-}
--- docs/.vuepress/.temp/pages/guide/register.html.js
+++ docs/.vuepress/.temp/pages/guide/register.html.js
@@ -1,16 +1,3 @@
 import comp from "D:/wwwroot/wanglei/ccnight_vuepress/docs/.vuepress/.temp/pages/guide/register.html.vue"
-const data = JSON.parse("{\"path\":\"/guide/register.html\",\"title\":\"注册账号\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"git\":{},\"filePathRelative\":\"guide/register.md\"}")
+const data = JSON.parse("{\"path\":\"/guide/register.html\",\"title\":\"注册账号\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"git\":{\"updatedTime\":1770619996000,\"contributors\":[{\"name\":\"wlphp\",\"username\":\"wlphp\",\"email\":\"613154514@qq.com\",\"commits\":1,\"url\":\"https://github.com/wlphp\"}],\"changelog\":[{\"hash\":\"094118ff2ee3da84eab8ae504d717fb2c7c0c042\",\"time\":1770619996000,\"email\":\"613154514@qq.com\",\"author\":\"wlphp\",\"message\":\"初始化魔板\"}]},\"filePathRelative\":\"guide/register.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
